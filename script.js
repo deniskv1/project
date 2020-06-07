2283c83f881c5993dbac8ec9e6affe17013c4a53
Denis <example@gmail.com>
2020-06-08 01:19:21 +0300

    second tast

--- script.js
+++ script.js
@@ -12,9 +12,28 @@ let appData = {
     savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов", ''),
-    a2 = prompt("Во сколько жто обойдется", '');
+for (let i = 0; i < 2; i++) {
+    let a1 = prompt("Введите обязательную статью расходов", ''),
+        a2 = prompt("Во сколько жто обойдется", '');
 
-appData.expenses = {a1: a2};
+    if (typeof(a1) === 'string' && typeof(a1) != null && typeof(a2) != null 
+    && a1 != '' && a2 != '' && a1.length < 50){
+        console.log("done");
+        appData.expenses[a1] = a2;
+    } else {
+        
+    }   
+}
 
-alert(appData.budget / 30);
+appData.moneyPerDay = appData.budget / 30;
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Что-то пошло не так");
+}
