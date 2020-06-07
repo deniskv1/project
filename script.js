140a89e83045f33269d15a14313a79129949ef52
Denis <example@gmail.com>
2020-06-07 22:59:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone["age"]);
+
+let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
+
+console.log(arr[1]);
+
+//alert("Hello world");
+
+// let ansver = confirm("Are you here?");
+
+// console.log(ansver);
+
+//let ansver = +prompt("Есть ли вам 18?", "Да");
+// console.log(typeof(arr));
+
+// console.log("arr" + " - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
