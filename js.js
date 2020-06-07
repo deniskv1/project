5933b55bed738d375c964f4ecf0483242e80e3ea
Denis <example@gmail.com>
2020-06-08 00:50:48 +0300

    functons

---
+++ js.js
@@ -0,0 +1,24 @@
+// function showFirstMessage(text) {
+//     alert(text);
+// }
+
+// showFirstMessage("privet");
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+let calc = (a, b) => a + b;
+
+console.log(calc(4, 3));
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+
+let anotherNum = retVar();
+console.log(anotherNum);
+
+let str = "test";
+console.log(str.length);
